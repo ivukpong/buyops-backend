2f0105a8975d37cbb03099c0278d8c36f0f17764
ivukpong <iniobong10000@gmail.com>
2026-02-02 20:21:26 +0100

    port fix

--- src/main.ts
+++ src/main.ts
@@ -46,7 +46,7 @@ async function bootstrap() {
   );
 
   const port = process.env.PORT || 4000;
- await app.listen(port, '0.0.0.0');
+  await app.listen(port);
   console.log(`Server listening on ${port}`);
 }
 
